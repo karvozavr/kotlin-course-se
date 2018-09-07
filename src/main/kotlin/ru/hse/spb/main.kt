a1567359b5171a9ff80824bf635b0786d0777b06
karvozavr <karvozavr@gmail.com>
2018-09-07 14:01:51 +0300

    Working solution. Added tests.

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,84 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
+import kotlin.collections.ArrayList
 
-    return words.joinToString(separator = " ")
+/**
+ * Solution class for codeforces 701E.
+ *
+ * @param adjacencyList adjacency list of the tree
+ * @param univ bool mask: is i'th vertex a university
+ * @param univNumber amount of universities
+ */
+class Solution(private val adjacencyList: Array<ArrayList<Int>>,
+               private val univ: Array<Boolean>,
+               private val univNumber: Int) {
+
+    private val vertexNumber = adjacencyList.size
+    private val subtreeUnivNumber = Array(vertexNumber) { _ -> 0 }
+    private val used = Array(vertexNumber) { _ -> false }
+
+    /**
+     * Finds answer to problem.
+     *
+     * @return answer to problem
+     */
+    fun solve(): Long {
+        dfs(0)
+
+        var answer: Long = 0
+        for (vertex in 0 until vertexNumber) {
+            answer += Math.min(subtreeUnivNumber[vertex], univNumber - subtreeUnivNumber[vertex])
+        }
+
+        return answer
+    }
+
+    /**
+     * Depth-first search calculating number of universities in a subtree of each vertex.
+     *
+     * @param vertex vertex to start dfs from
+     */
+    private fun dfs(vertex: Int) {
+        subtreeUnivNumber[vertex] = if (univ[vertex]) 1 else 0
+        used[vertex] = true
+
+        for (neighbour in adjacencyList[vertex]) {
+            if (!used[neighbour]) {
+                dfs(neighbour)
+                subtreeUnivNumber[vertex] += subtreeUnivNumber[neighbour]
+            }
+        }
+    }
+}
+
+/**
+ * Builds {@Link Solution} object from given input.
+ *
+ * @param input input source
+ */
+fun buildSolutionFromInput(input: Scanner): Solution {
+    val n = input.nextInt()
+    val k = input.nextInt()
+
+    val univ = Array(n) { _ -> false }
+    val adjacencyList = Array<ArrayList<Int>>(n) { _ -> ArrayList() }
+
+    for (i in 0 until 2 * k) {
+        univ[input.nextInt() - 1] = true
+    }
+
+    for (i in 0 until n - 1) {
+        val v = input.nextInt() - 1
+        val u = input.nextInt() - 1
+        adjacencyList[v].add(u)
+        adjacencyList[u].add(v)
+    }
+
+    return Solution(adjacencyList, univ, 2 * k)
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val solution = buildSolutionFromInput(Scanner(System.`in`))
+    println(solution.solve())
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -2,10 +2,22 @@ package ru.hse.spb
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class TestSource {
+
+    private val test1 = "7 2\n1 5 6 2\n1 3\n3 2\n4 5\n3 7\n4 3\n4 6"
+    private val test2 = "9 3\n3 2 1 6 5 9\n8 9\n3 2\n2 7\n3 4\n7 6\n4 5\n2 1\n2 8"
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun statementsTest1() {
+        val solution = buildSolutionFromInput(Scanner(test1))
+        assertEquals(6, solution.solve())
+    }
+
+    @Test
+    fun statementsTest2() {
+        val solution = buildSolutionFromInput(Scanner(test2))
+        assertEquals(9, solution.solve())
     }
 }
