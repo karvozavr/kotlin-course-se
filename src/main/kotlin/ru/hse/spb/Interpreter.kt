6d9889968696be3972e8d52c8fcbcc1d8a7cffef
karvozavr <karvozavr@gmail.com>
2018-09-16 16:33:23 +0300

    Start implementing interpreter

---
+++ src/main/kotlin/ru/hse/spb/Interpreter.kt
@@ -0,0 +1,63 @@
+package ru.hse.spb
+
+class Interpreter : ASTNodeVisitor<Unit> {
+    override fun visit(node: Block) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Function) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: VariableDeclaration) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Expression) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: While) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: If) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Assignment) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Return) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Identifier) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: ParameterNames) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: FunctionCall) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: BinaryExpression) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Literal) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: Arguments) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: ASTNode) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
