0d6e4d4028fca486ccfbc55a2360f3284dd3b3aa
karvozavr <karvozavr@gmail.com>
2018-09-18 01:43:10 +0300

    Start implementing interpreter

--- src/main/kotlin/ru/hse/spb/ASTNode.kt
+++ src/main/kotlin/ru/hse/spb/ASTNode.kt
@@ -2,48 +2,46 @@ package ru.hse.spb
 
 import java.util.concurrent.locks.Condition
 
-interface ASTNodeVisitor<out T> {
-    fun visit(node: Block): T
-    fun visit(node: Function): T
-    fun visit(node: VariableDeclaration): T
-    fun visit(node: Expression): T
-    fun visit(node: While): T
-    fun visit(node: If): T
-    fun visit(node: Assignment): T
-    fun visit(node: Return): T
-    fun visit(node: Identifier): T
-    fun visit(node: ParameterNames): T
-    fun visit(node: FunctionCall): T
-    fun visit(node: BinaryExpression): T
-    fun visit(node: Literal): T
-    fun visit(node: Arguments): T
-    fun visit(node: ASTNode): T
+interface ASTNodeVisitor<out StatementResult, out ExpressionResult> {
+    fun visit(node: Block): StatementResult
+    fun visit(node: Function): StatementResult
+    fun visit(node: VariableDeclaration): StatementResult
+    fun visit(node: While): StatementResult
+    fun visit(node: If): StatementResult
+    fun visit(node: Assignment): StatementResult
+    fun visit(node: Return): StatementResult
+    fun visit(node: Expression): ExpressionResult
+    fun visit(node: Identifier): ExpressionResult
+    fun visit(node: FunctionCall): ExpressionResult
+    fun visit(node: BinaryExpression): ExpressionResult
+    fun visit(node: Literal): ExpressionResult
+    fun visit(node: ASTNode) {}
 }
 
 interface ASTNode {
-    fun accept(visitor: ASTNodeVisitor<*>)
+    fun accept(visitor: ASTNodeVisitor<*, *>)
 }
 
 data class Block(val statements: List<Statement>) : ASTNode {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 interface Statement : ASTNode {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
-data class Function(val identifier: Identifier, val parameterNames: ParameterNames, val body: Block) : Statement {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+data class Function(val name: Identifier, val parameterNames: ParameterNames, val body: Block) : Statement {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class VariableDeclaration(val name: Identifier, val value: Expression) : Statement {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
@@ -51,25 +49,25 @@ data class VariableDeclaration(val name: Identifier, val value: Expression) : St
 interface Expression : Statement
 
 data class While(val condition: Expression, val body: Block) : Statement {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class If(val condition: Expression, val ifTrue: Block, val ifFalse: Block = Block(emptyList())) : Statement {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class Assignment(val identifier: Identifier, val value: Expression) : Statement {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class Return(val value: Expression) : Statement {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
@@ -77,19 +75,19 @@ data class Return(val value: Expression) : Statement {
 data class Identifier(val name: String) : Expression {
     var value: Int = 0
 
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class ParameterNames(val params: List<Identifier> = emptyList()) : ASTNode {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class FunctionCall(val name: Identifier, val args: Arguments) : Expression {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
@@ -111,19 +109,19 @@ data class BinaryExpression(val leftArgument: Expression, val rightArgument: Exp
         OR({ x, y -> if (x != 0 || y != 0) 1 else 0 })
     }
 
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class Literal(val value: Int) : Expression {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
 
 data class Arguments(val arguments: List<Expression>) : ASTNode {
-    override fun accept(visitor: ASTNodeVisitor<*>) {
+    override fun accept(visitor: ASTNodeVisitor<* ,*>) {
         visitor.visit(this)
     }
 }
--- src/main/kotlin/ru/hse/spb/Interpreter.kt
+++ src/main/kotlin/ru/hse/spb/Interpreter.kt
@@ -1,63 +1,165 @@
 package ru.hse.spb
 
-class Interpreter : ASTNodeVisitor<Unit> {
-    override fun visit(node: Block) {
+class Interpreter {
+
+    private val scope: Scope = Scope()
+    private val returnValue: ReturnValue = ReturnValue()
+
+    fun runInterpreter(program: Block) {
+        visit(program)
+    }
+
+    fun visit(node: Block) {
+        scope.openScope()
+        for (statement in node.statements) {
+            visit(statement)
+        }
+        scope.closeScope()
+    }
+
+    fun visit(node: Statement) {
+        return when (node) {
+            is Function -> visit(node)
+            is VariableDeclaration -> visit(node)
+            is While -> visit(node)
+            is If -> visit(node)
+            is Assignment -> visit(node)
+            is Return -> visit(node)
+            else -> throw IllegalStateException()
+        }
+    }
+
+    fun visit(node: Function) {
+        scope.declareFunction(node)
+    }
+
+    fun visit(node: VariableDeclaration) {
+        scope.declareVariable(node.name.name, visit(node.value))
+    }
+
+    fun visit(node: While) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(node: Function) {
+    fun visit(node: If) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(node: VariableDeclaration) {
+    fun visit(node: Assignment) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(node: Expression) {
+    fun visit(node: Return) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(node: While) {
+    fun visit(node: Expression): Int {
+        return when (node) {
+            is Identifier -> visit(node)
+            is FunctionCall -> visit(node)
+            is BinaryExpression -> visit(node)
+            is Literal -> visit(node)
+            else -> throw IllegalStateException()
+        }
+    }
+
+    fun visit(node: Identifier): Int {
+        return scope.lookupVariable(node.name)
+                ?: throw InterpreterError("Unknown variable name \"${node.name}\".")
+    }
+
+    fun visit(node: FunctionCall): Int {
+        val function: Function = scope.lookupFunction(node.name.name)
+                ?: throw InterpreterError("Unknown variable name \"${node.name}\".")
+        scope.openScope()
+        for ((argument, name) in node.args.arguments.zip(function.parameterNames.params.map { p -> p.name })) {
+            scope.declareVariable(name, visit(argument))
+        }
+        visit(function.body)
+        scope.closeScope()
+        TODO() // Break runtime on return
+        return returnValue.get()
+    }
+
+    fun visit(node: BinaryExpression): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(node: If) {
+    fun visit(node: Literal): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun visit(node: Assignment) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class InterpreterError(message: String) : Exception(message)
+
+    private class ReturnValue {
+        private var value: Int = 0
+
+        fun get(): Int {
+            val result = value
+            value = 0
+            return result
+        }
+
+        fun set(newValue: Int) {
+            value = newValue
+        }
     }
 
-    override fun visit(node: Return) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private class Scope {
+        private val variables = ArrayList<HashMap<String, Int>>()
+        private val functions = ArrayList<HashMap<String, Function>>()
 
-    override fun visit(node: Identifier) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        fun openScope() {
+            variables.add(HashMap())
+            functions.add(HashMap())
+        }
 
-    override fun visit(node: ParameterNames) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        fun closeScope() {
+            variables.dropLast(1)
+            functions.dropLast(1)
+        }
 
-    override fun visit(node: FunctionCall) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        fun lookupVariable(symbol: String): Int? {
+            for (scope in variables.asReversed()) {
+                if (symbol in scope) {
+                    return scope[symbol]
+                }
+            }
+            return null
+        }
 
-    override fun visit(node: BinaryExpression) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        fun lookupFunction(symbol: String): Function? {
+            for (scope in functions.asReversed()) {
+                if (symbol in scope) {
+                    return scope[symbol]
+                }
+            }
+            return null
+        }
 
-    override fun visit(node: Literal) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        fun declareFunction(function: Function) {
+            val symbol = function.name.name
+            if (symbol in functions.last()) {
+                throw InterpreterError("Function \"$symbol\" already declared in current scope.")
+            }
+            functions.last()["ss"] = function
+        }
 
-    override fun visit(node: Arguments) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        fun declareVariable(symbol: String, value: Int) {
+            if (symbol in variables.last()) {
+                throw InterpreterError("Variable \"$symbol\" already declared in current scope.")
+            }
+            variables.last()["ss"] = value
+        }
 
-    override fun visit(node: ASTNode) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        fun updateVariable(symbol: String, value: Int) {
+            for (scope in variables.asReversed()) {
+                if (symbol in scope) {
+                    scope[symbol] = value
+                    return
+                }
+            }
+            throw InterpreterError("Unknown variable name \"$symbol\".")
+        }
     }
 }
