aed3b99b9cba3224ce02e2a131369fec9ee3b3e3
karvozavr <karvozavr@gmail.com>
2018-10-04 21:11:25 +0300

    init

---
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -0,0 +1,23 @@
+package ru.hse.spb
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.*
+
+class TestSource {
+
+    private val test1 = "7 2\n1 5 6 2\n1 3\n3 2\n4 5\n3 7\n4 3\n4 6"
+    private val test2 = "9 3\n3 2 1 6 5 9\n8 9\n3 2\n2 7\n3 4\n7 6\n4 5\n2 1\n2 8"
+
+    @Test
+    fun statementsTest1() {
+        val solution = buildSolutionFromInput(Scanner(test1))
+        assertEquals(6, solution.answer)
+    }
+
+    @Test
+    fun statementsTest2() {
+        val solution = buildSolutionFromInput(Scanner(test2))
+        assertEquals(9, solution.answer)
+    }
+}
